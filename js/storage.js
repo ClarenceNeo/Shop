422959a703ea39e701de5c659c3c90bfaa2007a2
ClarenceNeo <niuzhiyuan94@gmail.com>
2017-10-24 14:49:42 +0800

    add storage.js

---
+++ js/storage.js
@@ -0,0 +1,13 @@
+;(function () {
+
+  window.s = {};
+  s.get = function(key){
+    var json = localStorage.getItem(key);
+    return JSON.parse(json);
+  }
+
+  s.set = function(key, val){
+    var json = JSON.stringify(val);
+    localStorage.setItem(key, json);
+  }
+})();
